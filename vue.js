fecd9749a04cb5ba929e04751a706763cbc896c2
Sinka Gábor <hessor@gmail.com>
2019-05-31 18:12:17 +0200

    apiConfig

--- vue.js
+++ vue.js
@@ -1,7 +1,6 @@
-
-  Vue.component('city-item',{
-    props: ['city'],
-    template: `
+Vue.component('city-item', {
+  props: ['city'],
+  template: `
     <div class="card">
     <img src="./sunny.png" alt="city-weather">
     <div class="container">
@@ -12,32 +11,41 @@
     `
 });
 
-
-
 const app = new Vue({
-    el: '#app',
-    data: {
-        info:'',
-        localWeather: '',
-        cityIds: [7284840,7284841,7284842,7284843]
-      },
-    async mounted() {
-        this.info = await axios.get(`http://api.openweathermap.org/data/2.5/group?id=${this.concat}&units=metric&APPID=850970f506f7625ac19765050dbbc878`);
-        this.getLocation()   
-    }, computed: {
-        concat: function(){
-            return this.cityIds.join(',');
-        }
-    },methods: {
-        getLocation: function(){
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(this.getLocalWeather);
-              } else { 
-                  
-              }
-        },
-        getLocalWeather: async function (position){
-            this.localWeather =  await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=850970f506f7625ac19765050dbbc878`);
-        }
+  el: '#app',
+  data: {
+    info: '',
+    localWeather: '',
+    cityIds: apiConfig.cityIDs
+  },
+  async mounted() {
+    this.info = await axios.get(
+      `http://api.openweathermap.org/data/2.5/group?id=${
+        this.concat
+      }&units=metric&APPID=850970f506f7625ac19765050dbbc878`
+    );
+    this.getLocation();
+  },
+  computed: {
+    concat: function() {
+      return this.cityIds.join(',');
+    }
+  },
+  methods: {
+    getLocation: function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.getLocalWeather);
+      } else {
+      }
     },
-  });
+    getLocalWeather: async function(position) {
+      this.localWeather = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${
+          position.coords.latitude
+        }&lon=${
+          position.coords.longitude
+        }&units=metric&APPID=850970f506f7625ac19765050dbbc878`
+      );
+    }
+  }
+});
